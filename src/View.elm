52f4797ad637609f27ffeaf17a22a364ddfc5866
Rafael Epplee <raffomania@gmail.com>
2020-04-24 20:59:07 +0200

    more borders

--- src/View.elm
+++ src/View.elm
@@ -19,7 +19,10 @@ import Month
 import Msg exposing (..)
 import Util exposing (dictUpsert)
 
-borders = { bottom = 0, left = 0, right = 0, top = 0 }
+
+borders =
+    { bottom = 0, left = 0, right = 0, top = 0 }
+
 
 view : Model -> Html.Html Msg
 view model =
@@ -36,11 +39,12 @@ view model =
                     , transactionList model
                     ]
     in
-    layout [ Background.color Colors.bg]
-        (column [height fill, width fill]
+    layout [ Background.color Colors.bg ]
+        (column [ height fill, width fill ]
             [ navigation model
-            , column [ spacing 20, padding 20, centerX] page
-            , el [height fill, Background.color Colors.accent, width fill] none
+            , el [ Border.widthXY 0 4, Border.color Colors.bgAccent, width fill ]
+                (column [ spacing 20, padding 20, centerX ] page)
+            , el [ height fill, Background.color Colors.accent, width fill ] none
             ]
         )
 
@@ -58,7 +62,7 @@ navigation model =
         buttonStyle =
             [ height fill, padding 10 ]
     in
-    row [ spacing 10, Background.color Colors.accent, width fill, height (px 50), Border.color Colors.accent2 , Font.color Colors.bg, Border.widthEach {borders|bottom  = 4}, Border.color Colors.bgAccent]
+    row [ spacing 10, Background.color Colors.accent, width fill, height (px 50), Font.color Colors.bg ]
         [ row [ width <| fillPortion 2, height fill, spacing 10 ]
             [ Input.button [ width fill, Font.center, height fill ]
                 { onPress = Just PreviousMonth
