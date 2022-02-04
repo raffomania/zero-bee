eac2c0c861a0eaaab0529d264595bfad83db5bf1
Rafael Epplée <hello@rafa.ee>
2022-02-05 00:16:59 +0100

    fix tauri-build version, add file selection

--- src/index_tauri.js
+++ src/index_tauri.js
@@ -1,7 +1,6 @@
 import { Elm } from "./Main.elm";
 
 import { save } from "@tauri-apps/api/dialog";
-import { writeText } from "@tauri-apps/api/clipboard";
 
 window.addEventListener("DOMContentLoaded", () => {
     let app = Elm.Main.init({
@@ -9,6 +8,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     app.ports.downloadBackup.subscribe(() => {
-        save().then(console.log);
+        var year = new Date().getFullYear();
+        var month = parseInt(new Date().getMonth(), 10) + 1;
+
+        save({
+            defaultPath: `zero_bee_backup_${year}_${month}.json`,
+            filters: [{ name: "JSON Files", extensions: ["json"] }],
+        }).then(console.log);
     });
 });
